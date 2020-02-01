208544061286874400e6bc2d72b955df8775ccdb
Chris Lin <christophercklin@gmail.com>
2020-02-01 14:14:29 -0500

    Volcano scene

---
+++ StarWarsVR/Assets/Orbit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = (target.position + new Vector3(0, 0, 0)) - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        Quaternion current = transform.localRotation;
+
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+        transform.Translate(10* Time.deltaTime, 0, 0);
+    }
+}
