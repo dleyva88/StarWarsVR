30fbb379eed1a9160f575c1a455bfc4ccc7d7cbd
Chris Lin <christophercklin@gmail.com>
2020-02-02 20:09:36 -0500

    2nd Mode
    Created new scene for game mode 2.
    
    The script will create new droids when it is destroyed (collides with lightsaber). A variable is set to keep track of current "wave". Once maxWave has been reached, do not create anymore droids. Each droid will spawn 2 new droids in a random location in a 50 unit radius around center point (500, 0, 500).

---
+++ StarWarsVR/Assets/behavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class behavior : MonoBehaviour
+{
+    public int maxWave = 10;
+    int curWave = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.LookAt(GameObject.Find("player").transform);
+
+        transform.position += transform.forward * 2.0f * Time.smoothDeltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject.name.Equals("player"))
+        {
+            Debug.Log("The player got hit!");
+
+            Destroy(this.gameObject);
+        }
+        else if (other.transform.gameObject.name.Equals("Lightsaber"))
+        {
+            Debug.Log("Hit lightsaber");
+
+            if (curWave < maxWave)
+            {
+                float newX = Random.Range(-50f, 50f);
+                float newZ = Random.Range(-50f, 50f);
+
+                Vector3 newLocation = new Vector3(500 + newX, 0, 500 + newZ);
+                
+                var newDroid = Instantiate(this.gameObject, newLocation, this.transform.rotation);
+                newDroid.GetComponent<behavior>().curWave++;
+
+                newX = Random.Range(-50f, 50f);
+                newZ = Random.Range(-50f, 50f);
+
+                newLocation = new Vector3(500 + newX, 0, 500 + newZ);
+
+                newDroid = Instantiate(this.gameObject, newLocation, this.transform.rotation);
+                newDroid.GetComponent<behavior>().curWave++;
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+}
