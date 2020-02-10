8b5200e3a3e47abba95779d1285f94c890c06604
dleyva88 <danleyva88@gmail.com>
2020-02-10 15:47:01 -0500

    Revert "Removing unused assets"
    This reverts commit 3b8fdb4687adafc54f525348741facfcac881108.

---
+++ StarWarsVR/Assets/Scripts/Controllers/VRRaycast.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+// https://circuitstream.com/blog/htc-vive-tutorial-raycasting/ 
+
+public class VRRaycast : MonoBehaviour
+{
+    protected RaycastHit rayHit;
+    protected GameObject hitObject;
+
+    void Awake()
+    {
+        //If attached to head, will return null (expected behavior)
+        //Input input = GetComponent()
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        //Check if raycast hits anything
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out rayHit))
+        {
+            //If the object is the same as the one we hit last frame
+            if (hitObject != null && hitObject == rayHit.transform.gameObject)
+            {
+                //Trigger "Stay" method on Interactable every frame we hit
+                RayStay(rayHit);
+            }
+
+            //We're still hitting something, but it's a new object
+            else
+            {
+                //Keep track of new object that we're hitting
+                hitObject = rayHit.transform.gameObject;
+            }
+        }
+        // We're still hitting something, but it's a new object
+        else
+        {
+            //Keep track of new object that we're hitting, and trigger the ray "Enter" method on Interactable
+            hitObject = rayHit.transform.gameObject;
+            RayEnter(rayHit);
+
+        }
+    }
+
+    protected void RayEnter(RaycastHit hit)
+    {
+
+    }
+
+
+    protected void RayStay(RaycastHit hit)
+    {
+
+
+    }
+
+
+    protected void RayExit()
+
+    {
+
+
+    }
+}
---
+++ StarWarsVR/Assets/StarWarz/Force/Scripts/Pushable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Pushable : MonoBehaviour
+{
+    public delegate void PushedHandler();
+    public PushedHandler Pushed = delegate { };
+    
+    private Rigidbody mRigidbody;
+    
+    void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Pusher pushScript = collider.GetComponentInParent<Pusher>();
+
+        if (pushScript)
+        {
+            Pushed();
+
+            mRigidbody.AddForce(pushScript.PushDirection * pushScript.PushForce);
+        }
+    }
+}
